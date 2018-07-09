8585449d239412962be89b330c77ae652fc441f3
Jake Neumann <thejakeneumann@gmail.com>
2018-07-09 18:58:18 -0500

    Move restaurant data to its own class

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
+  require('restaurant.php');
+  $restaurant = new Restaurant();
   $path = explode('/', $_SERVER['PATH_INFO']);
 
   switch($path[1]) {
     case "restaurant":
-      dump_restaurant_data($path[2]);
+      $restaurant->human_readable($path[2]);
+      break;
+    case "dashobard":
+      die("DASHBOARD ACCESSED");
       break;
     default:
       echo "INVALID ROUTE<br />";
@@ -12,55 +17,3 @@
       die();
       break;
   }
-
-function dump_restaurant_data($restaurant) {
-  $file = file_get_contents("./data/$restaurant.json");
-
-  if ($file == FALSE) {
-    echo "INVALID RESTAURANT<br />";
-    die();
-  }
-
-  $data = json_decode($file, true);
-
-  $html = "";
-  $html .= "<html>";
-  $html .= "<body>";
-  $html .= "<h1>{$data['name']}</h1>";
-  if (!empty($data['hours'])) {
-  $html .= "<div>";
-  $html .= "<h2>Hours</h2>";
-  $html .= $data['hours'];
-  $html .= "</div>";
-  }
-  $html .= "<div>";
-  $html .= "<h2>Phone</h2>";
-  $html .= $data['phone'];
-  $html .= "</div>";
-  $html .= "<div>";
-  $html .= "<h2>Website</h2>";
-  $html .= "<a href='{$data['website']}'>{$data['website']}</a>";
-  $html .= "</div>";
-  $html .= "<div>";
-  $html .= "<h2>Address</h2>";
-  $html .= "<p>{$data['address']['street']}</p>";
-  $html .= "<p>{$data['address']['city']},";
-  $html .= $data['address']['state'] . " ";
-  $html .= $data['address']['zip'] . "</p>";
-  $html .= "</div>";
-  $html .= "<div>";
-  $html .= "<h2>Menu</h2>";
-  foreach ($data['menu'] as $key => $section) {
-    $html .= "<h3>$key</h3>";
-    foreach ($section as $item) {
-      $html .= "<div>{$item['name']} {$item['price']}</div>";
-      $html .= "<div>{$item['description']}</div>";
-      $html .= "<hr />";
-    }
-  }
-  $html .= "</div>";
-  $html .= "</body>";
-  $html .= "</html>";
-
-  echo $html;
-}
---
+++ restaurant.php
@@ -0,0 +1,54 @@
+<?php
+  class Restaurant {
+    public function human_readable($restaurant) {
+      $file = file_get_contents("./data/$restaurant.json");
+
+      if ($file == FALSE) {
+        echo "INVALID RESTAURANT<br />";
+        die();
+      }
+
+      $data = json_decode($file, true);
+
+      $html = "";
+      $html .= "<html>";
+      $html .= "<body>";
+      $html .= "<h1>{$data['name']}</h1>";
+      if (!empty($data['hours'])) {
+      $html .= "<div>";
+      $html .= "<h2>Hours</h2>";
+      $html .= $data['hours'];
+      $html .= "</div>";
+      }
+      $html .= "<div>";
+      $html .= "<h2>Phone</h2>";
+      $html .= $data['phone'];
+      $html .= "</div>";
+      $html .= "<div>";
+      $html .= "<h2>Website</h2>";
+      $html .= "<a href='{$data['website']}'>{$data['website']}</a>";
+      $html .= "</div>";
+      $html .= "<div>";
+      $html .= "<h2>Address</h2>";
+      $html .= "<p>{$data['address']['street']}</p>";
+      $html .= "<p>{$data['address']['city']},";
+      $html .= $data['address']['state'] . " ";
+      $html .= $data['address']['zip'] . "</p>";
+      $html .= "</div>";
+      $html .= "<div>";
+      $html .= "<h2>Menu</h2>";
+      foreach ($data['menu'] as $key => $section) {
+        $html .= "<h3>$key</h3>";
+        foreach ($section as $item) {
+          $html .= "<div>{$item['name']} {$item['price']}</div>";
+          $html .= "<div>{$item['description']}</div>";
+          $html .= "<hr />";
+        }
+      }
+      $html .= "</div>";
+      $html .= "</body>";
+      $html .= "</html>";
+
+      echo $html;
+    }
+  }
