cb537006faabb9a1b752e8254fe28b4885fc3ad8
Jake Neumann <thejakeneumann@gmail.com>
2018-07-17 11:40:11 -0500

    Fix issue with routing if no path specified

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
   require('restaurant.php');
   $restaurant = new Restaurant();
-  $path = explode('/', $_SERVER['PATH_INFO']);
+
+  $path = explode('/', $_SERVER['REQUEST_URI']);
 
   switch($path[1]) {
     case "restaurant":
       $restaurant->human_readable($path[2]);
       break;
-    case "dashobard":
-      die("DASHBOARD ACCESSED");
+    case "dashboard":
+      switch($_SERVER['REQUEST_METHOD']) {
+        case "POST":
+          die("POST");
+          break;
+        default:
+          require_once('dashboard.php');
+          break;
+      }
       break;
     default:
       echo "INVALID ROUTE<br />";
