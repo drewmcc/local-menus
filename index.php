7de2c2bfbfc5f6df1aec6ff48948b62803db7fd8
Jake Neumann <thejakeneumann@gmail.com>
2018-08-20 17:32:45 -0500

    Clean up files

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
-  require('header.php');
+  require('src/header.php');
 
   $path = explode('/', $_SERVER['REQUEST_URI']);
 
   switch($path[1]) {
     case "restaurant":
       $restaurant = $path[2];
-      require('restaurant.php');
+      require('src/restaurant.php');
       break;
     case "edit":
       if (!is_null($path[2])) {
         $restaurant = json_decode(file_get_contents("./data/{$path[2]}.json"), true);
-        require_once('dashboard.php');
+        require_once('src/dashboard.php');
       } else {
         die('No Restaurant Given');
       }
       break;
     case "dashboard":
-      require_once('dashboard.php');
+      require_once('src/dashboard.php');
       break;
     default:
       echo "INVALID ROUTE<br />";
